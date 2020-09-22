5d0fd24fd9cae76958640cdaaf6be2a51d8edea3
MuthiaDwirza <71259803+MuthiaDwirza@users.noreply.github.com>
2020-09-22 12:38:47 +0700

    Add files via upload

---
+++ pratikum1.php
@@ -0,0 +1,45 @@
+<html> 
+    <head> 
+        <title>Pratikum</title> 
+    </head> 
+    <body> <h2><u>PIRAMIDA</u></h2>
+<?php
+	$star=5;
+	for($a=1; $a<=$star; $a++){
+	for($i=1; $i<=$a; $i++){
+		echo " &nbsp";
+	}
+	for($c=$star; $c>=$a; $c-=1){
+		echo "*";
+	}
+	echo "<br>";
+	}
+?>
+<br><br><br>
+<?php
+	$star=6;
+	for($a=1; $a<$star; $a++){
+	for($b=1; $b<=$a; $b++){
+		echo "&nbsp";
+	}
+	for($c=$star; $c>=$a; $c-=1){
+		echo "*";
+	}
+	echo "<br>";
+	}
+?>
+<br><br><br>
+<?php
+	$star=1;
+	for($a=5; $a>=$star; $a--){
+	for($b=5; $b>=$a; $b--){
+		echo "&nbsp";
+	}
+	for($c=$star; $c<=$a; $c++){
+		echo $c;
+	}
+	echo "<br>";
+	}
+?>
+</body> 
+</html>
